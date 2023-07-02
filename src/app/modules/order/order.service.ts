a0a489b4bf5ebd42c0128fa8114dc20b2ee43c27
ahmad muas <amaahmadmusa@gmail.com>
2023-07-02 22:21:39 +0600

    transaction is added

--- src/app/modules/order/order.service.ts
+++ src/app/modules/order/order.service.ts
@@ -2,15 +2,34 @@ import httpStatus from 'http-status';
 import ApiError from '../../../errors/ApiError';
 import { IOrder } from './order.interface';
 import Order from './order.model';
+import mongoose from 'mongoose';
 
-const createOrder = (userData: IOrder): Promise<IOrder | null> => {
-  const createdOrder = Order.create(userData);
+const createOrder = async (orderData: IOrder): Promise<IOrder | null> => {
+  const ar = [];
+  const session = await mongoose.startSession();
+  try {
+    session.startTransaction();
 
-  if (!createdOrder) {
-    throw new ApiError(httpStatus.EXPECTATION_FAILED, 'failed to create Order');
+    const createdOrder = Order.create(orderData);
+
+    if (!createdOrder) {
+      throw new ApiError(
+        httpStatus.EXPECTATION_FAILED,
+        'failed to create Order'
+      );
+    } else {
+      ar.push(createdOrder);
+    }
+
+    await session.commitTransaction();
+    await session.endSession();
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw error;
   }
 
-  return createdOrder;
+  return ar[0];
 };
 
 const getAllOrders = async (): Promise<IOrder[] | null> => {
